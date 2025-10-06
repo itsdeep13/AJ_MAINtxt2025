3d1be46660389b2e617c1042a5b02e3656563070
itsdeep13 <80030947+itsdeep13@users.noreply.github.com>
2025-10-06 10:56:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    api_id = 12283173515
-    api_hash = "be786a8240abf6e12283173515e2f833"
-    bot_token = "8113578720:AAGuDsG3Gqa6XkwurIFbQRX5y3LwrtbTeU8"
+    api_id = 27473563
+    api_hash = "bc2ea0765ac96bb474891b0243f44390"
+    bot_token = "8280777980:AAH8H8NAI5a4xlk62VPWJmXkGM3UreRwXxE"
     pwtoken = "sdt1"
  
